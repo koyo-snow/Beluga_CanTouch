00713a6b5aba2fe5664d2d7dd929423f0a389cae
momiji <108613029+koyo-snow@users.noreply.github.com>
2022-11-28 10:40:56 +0900

    delete unnecessary js file

--- js/main1.js
+++ js/main1.js
@@ -3,7 +3,7 @@
 
   // マウスの座標を取得する。
   let mouseX = 0, //マウスのX座標
-      mouseY = 0; //マウスのY座標
+    mouseY = 0; //マウスのY座標
 
   $(document).on("mousemove", function (e) {
     mouseX = e.pageX;
@@ -32,51 +32,51 @@
     let CanTouchBelugaShape = new createjs.Shape();
     CanTouchBelugaShape.alpha = 0.01;
     ////// 輪郭確認用
-    CanTouchBelugaShape.alpha = 1;
+    // CanTouchBelugaShape.alpha = 1;
     CanTouchBelugaShape.graphics.beginFill("#ffffff");
 
-    CanTouchBelugaShape.graphics.moveTo(122.00,90.00);
-    CanTouchBelugaShape.graphics.lineTo(88.00,101.00);
-    CanTouchBelugaShape.graphics.lineTo(72.00,118.00);
-    CanTouchBelugaShape.graphics.lineTo(67.00,135.00);
-    CanTouchBelugaShape.graphics.lineTo(69.00,159.00);
-    CanTouchBelugaShape.graphics.lineTo(74.00,187.00);
-    CanTouchBelugaShape.graphics.lineTo(80.00,215.00);
-    CanTouchBelugaShape.graphics.lineTo(87.00,253.00);
-    CanTouchBelugaShape.graphics.lineTo(90.00,295.00);
-    CanTouchBelugaShape.graphics.lineTo(92.00,344.00);
-    CanTouchBelugaShape.graphics.lineTo(97.00,384.00);
-    CanTouchBelugaShape.graphics.lineTo(113.00,447.00);
-    CanTouchBelugaShape.graphics.lineTo(127.00,505.00);
-    CanTouchBelugaShape.graphics.lineTo(129.00,520.00);
-    CanTouchBelugaShape.graphics.lineTo(129.00,553.00);
-    CanTouchBelugaShape.graphics.lineTo(116.00,605.00);
-    CanTouchBelugaShape.graphics.lineTo(100.00,663.00);
-    CanTouchBelugaShape.graphics.lineTo(82.00,713.00 );
-    CanTouchBelugaShape.graphics.lineTo(47.00,798.00 );
-    CanTouchBelugaShape.graphics.lineTo(674.00,799.00);
-    CanTouchBelugaShape.graphics.lineTo(636.00,711.00);
-    CanTouchBelugaShape.graphics.lineTo(612.00,662.00);
-    CanTouchBelugaShape.graphics.lineTo(592.00,621.00);
-    CanTouchBelugaShape.graphics.lineTo(570.00,584.00);
-    CanTouchBelugaShape.graphics.lineTo(549.00,548.00);
-    CanTouchBelugaShape.graphics.lineTo(529.00,517.00);
-    CanTouchBelugaShape.graphics.lineTo(508.00,478.00);
-    CanTouchBelugaShape.graphics.lineTo(496.00,447.00);
-    CanTouchBelugaShape.graphics.lineTo(482.00,418.00);
-    CanTouchBelugaShape.graphics.lineTo(466.00,372.00);
-    CanTouchBelugaShape.graphics.lineTo(451.00,334.00);
-    CanTouchBelugaShape.graphics.lineTo(434.00,290.00);
-    CanTouchBelugaShape.graphics.lineTo(406.00,240.00);
-    CanTouchBelugaShape.graphics.lineTo(366.00,167.00);
-    CanTouchBelugaShape.graphics.lineTo(337.00,124.00);
-    CanTouchBelugaShape.graphics.lineTo(305.00,88.00);
-    CanTouchBelugaShape.graphics.lineTo(275.00,65.00);
-    CanTouchBelugaShape.graphics.lineTo(247.00,52.00);
-    CanTouchBelugaShape.graphics.lineTo(214.00,46.00);
-    CanTouchBelugaShape.graphics.lineTo(185.00,49.00);
-    CanTouchBelugaShape.graphics.lineTo(158.00,63.00);
-    CanTouchBelugaShape.graphics.lineTo(122.00,90.00);
+    CanTouchBelugaShape.graphics.moveTo(122.00, 90.00);
+    CanTouchBelugaShape.graphics.lineTo(88.00, 101.00);
+    CanTouchBelugaShape.graphics.lineTo(72.00, 118.00);
+    CanTouchBelugaShape.graphics.lineTo(67.00, 135.00);
+    CanTouchBelugaShape.graphics.lineTo(69.00, 159.00);
+    CanTouchBelugaShape.graphics.lineTo(74.00, 187.00);
+    CanTouchBelugaShape.graphics.lineTo(80.00, 215.00);
+    CanTouchBelugaShape.graphics.lineTo(87.00, 253.00);
+    CanTouchBelugaShape.graphics.lineTo(90.00, 295.00);
+    CanTouchBelugaShape.graphics.lineTo(92.00, 344.00);
+    CanTouchBelugaShape.graphics.lineTo(97.00, 384.00);
+    CanTouchBelugaShape.graphics.lineTo(113.00, 447.00);
+    CanTouchBelugaShape.graphics.lineTo(127.00, 505.00);
+    CanTouchBelugaShape.graphics.lineTo(129.00, 520.00);
+    CanTouchBelugaShape.graphics.lineTo(129.00, 553.00);
+    CanTouchBelugaShape.graphics.lineTo(116.00, 605.00);
+    CanTouchBelugaShape.graphics.lineTo(100.00, 663.00);
+    CanTouchBelugaShape.graphics.lineTo(82.00, 713.00);
+    CanTouchBelugaShape.graphics.lineTo(47.00, 798.00);
+    CanTouchBelugaShape.graphics.lineTo(674.00, 799.00);
+    CanTouchBelugaShape.graphics.lineTo(636.00, 711.00);
+    CanTouchBelugaShape.graphics.lineTo(612.00, 662.00);
+    CanTouchBelugaShape.graphics.lineTo(592.00, 621.00);
+    CanTouchBelugaShape.graphics.lineTo(570.00, 584.00);
+    CanTouchBelugaShape.graphics.lineTo(549.00, 548.00);
+    CanTouchBelugaShape.graphics.lineTo(529.00, 517.00);
+    CanTouchBelugaShape.graphics.lineTo(508.00, 478.00);
+    CanTouchBelugaShape.graphics.lineTo(496.00, 447.00);
+    CanTouchBelugaShape.graphics.lineTo(482.00, 418.00);
+    CanTouchBelugaShape.graphics.lineTo(466.00, 372.00);
+    CanTouchBelugaShape.graphics.lineTo(451.00, 334.00);
+    CanTouchBelugaShape.graphics.lineTo(434.00, 290.00);
+    CanTouchBelugaShape.graphics.lineTo(406.00, 240.00);
+    CanTouchBelugaShape.graphics.lineTo(366.00, 167.00);
+    CanTouchBelugaShape.graphics.lineTo(337.00, 124.00);
+    CanTouchBelugaShape.graphics.lineTo(305.00, 88.00);
+    CanTouchBelugaShape.graphics.lineTo(275.00, 65.00);
+    CanTouchBelugaShape.graphics.lineTo(247.00, 52.00);
+    CanTouchBelugaShape.graphics.lineTo(214.00, 46.00);
+    CanTouchBelugaShape.graphics.lineTo(185.00, 49.00);
+    CanTouchBelugaShape.graphics.lineTo(158.00, 63.00);
+    CanTouchBelugaShape.graphics.lineTo(122.00, 90.00);
 
     stageCanTouchBeluga.addChild(CanTouchBelugaShape);
 
@@ -102,7 +102,7 @@
       } else {
         document.body.style.cursor = "pointer";
       }
-      
+
       // Stageの描画を更新する。
       stageCanTouchBeluga.update();
     }
